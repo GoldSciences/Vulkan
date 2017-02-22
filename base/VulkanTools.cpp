f1fa81be452ef70e609ca8d32a915d2fb5905ec2
Pablo <pabloaz@gmail.com>
2017-02-22 00:28:11 -0300

    * Removed an insane amount of lines caused by bad and outdated advice.

--- base/VulkanTools.cpp
+++ base/VulkanTools.cpp
@@ -102,7 +102,7 @@ namespace vks
 			VkPipelineStageFlags dstStageMask)
 		{
 			// Create an image barrier object
-			VkImageMemoryBarrier imageMemoryBarrier = vks::initializers::imageMemoryBarrier();
+			VkImageMemoryBarrier	imageMemoryBarrier	= vks::initializers::imageMemoryBarrier();
 			imageMemoryBarrier.oldLayout = oldImageLayout;
 			imageMemoryBarrier.newLayout = newImageLayout;
 			imageMemoryBarrier.image = image;
@@ -111,91 +111,38 @@ namespace vks
 			// Source layouts (old)
 			// Source access mask controls actions that have to be finished on the old layout
 			// before it will be transitioned to the new layout
+			VkAccessFlags			& srcMask			= imageMemoryBarrier.srcAccessMask;
 			switch (oldImageLayout)
 			{
-			case VK_IMAGE_LAYOUT_UNDEFINED:
-				// Image layout is undefined (or does not matter)
-				// Only valid as initial layout
-				// No flags required, listed only for completeness
-				imageMemoryBarrier.srcAccessMask = 0;
-				break;
-
-			case VK_IMAGE_LAYOUT_PREINITIALIZED:
-				// Image is preinitialized
-				// Only valid as initial layout for linear images, preserves memory contents
-				// Make sure host writes have been finished
-				imageMemoryBarrier.srcAccessMask = VK_ACCESS_HOST_WRITE_BIT;
-				break;
-
-			case VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL:
-				// Image is a color attachment
-				// Make sure any writes to the color buffer have been finished
-				imageMemoryBarrier.srcAccessMask = VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT;
-				break;
-
-			case VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL:
-				// Image is a depth/stencil attachment
-				// Make sure any writes to the depth/stencil buffer have been finished
-				imageMemoryBarrier.srcAccessMask = VK_ACCESS_DEPTH_STENCIL_ATTACHMENT_WRITE_BIT;
-				break;
-
-			case VK_IMAGE_LAYOUT_TRANSFER_SRC_OPTIMAL:
-				// Image is a transfer source 
-				// Make sure any reads from the image have been finished
-				imageMemoryBarrier.srcAccessMask = VK_ACCESS_TRANSFER_READ_BIT;
-				break;
-
-			case VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL:
-				// Image is a transfer destination
-				// Make sure any writes to the image have been finished
-				imageMemoryBarrier.srcAccessMask = VK_ACCESS_TRANSFER_WRITE_BIT;
-				break;
-
-			case VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL:
-				// Image is read by a shader
-				// Make sure any shader reads from the image have been finished
-				imageMemoryBarrier.srcAccessMask = VK_ACCESS_SHADER_READ_BIT;
-				break;
+			case VK_IMAGE_LAYOUT_UNDEFINED							: srcMask = 0;															break;	// Image layout is undefined (or does not matter). Only valid as initial layout. No flags required, listed only for completeness
+			case VK_IMAGE_LAYOUT_PREINITIALIZED						: srcMask = VK_ACCESS_HOST_WRITE_BIT;									break;	// Image is preinitialized. Only valid as initial layout for linear images, preserves memory contents.  Make sure host writes have been finished
+			case VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL			: srcMask = VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT;						break;	// Image is a color attachment.			Make sure any writes to the color buffer have been finished
+			case VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL	: srcMask = VK_ACCESS_DEPTH_STENCIL_ATTACHMENT_WRITE_BIT;				break;	// Image is a depth/stencil attachment.	Make sure any writes to the depth/stencil buffer have been finished
+			case VK_IMAGE_LAYOUT_TRANSFER_SRC_OPTIMAL				: srcMask = VK_ACCESS_TRANSFER_READ_BIT;								break;	// Image is a transfer source.			Make sure any reads from the image have been finished
+			case VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL				: srcMask = VK_ACCESS_TRANSFER_WRITE_BIT;								break;	// Image is a transfer destination.		Make sure any writes to the image have been finished
+			case VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL			: srcMask = VK_ACCESS_SHADER_READ_BIT;									break;	// Image is read by a shader.			Make sure any shader reads from the image have been finished
 			}
 
 			// Target layouts (new)
 			// Destination access mask controls the dependency for the new image layout
+			VkAccessFlags			& dstMask			= imageMemoryBarrier.dstAccessMask;
 			switch (newImageLayout)
 			{
-			case VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL:
-				// Image will be used as a transfer destination
-				// Make sure any writes to the image have been finished
-				imageMemoryBarrier.dstAccessMask = VK_ACCESS_TRANSFER_WRITE_BIT;
-				break;
-
-			case VK_IMAGE_LAYOUT_TRANSFER_SRC_OPTIMAL:
-				// Image will be used as a transfer source
-				// Make sure any reads from and writes to the image have been finished
-				imageMemoryBarrier.srcAccessMask = imageMemoryBarrier.srcAccessMask | VK_ACCESS_TRANSFER_READ_BIT;
-				imageMemoryBarrier.dstAccessMask = VK_ACCESS_TRANSFER_READ_BIT;
-				break;
-
-			case VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL:
-				// Image will be used as a color attachment
-				// Make sure any writes to the color buffer have been finished
-				imageMemoryBarrier.srcAccessMask = VK_ACCESS_TRANSFER_READ_BIT;
-				imageMemoryBarrier.dstAccessMask = VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT;
-				break;
-
-			case VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL:
-				// Image layout will be used as a depth/stencil attachment
-				// Make sure any writes to depth/stencil buffer have been finished
-				imageMemoryBarrier.dstAccessMask = imageMemoryBarrier.dstAccessMask | VK_ACCESS_DEPTH_STENCIL_ATTACHMENT_WRITE_BIT;
-				break;
-
-			case VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL:
-				// Image will be read in a shader (sampler, input attachment)
-				// Make sure any writes to the image have been finished
+			case VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL				: dstMask = VK_ACCESS_TRANSFER_WRITE_BIT;								break;	// Image will be used as a transfer destination.			Make sure any writes to the image have been finished	
+			case VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL	: dstMask = dstMask | VK_ACCESS_DEPTH_STENCIL_ATTACHMENT_WRITE_BIT;		break;	// Image layout will be used as a depth/stencil attachment.	Make sure any writes to depth/stencil buffer have been finished
+			case VK_IMAGE_LAYOUT_TRANSFER_SRC_OPTIMAL				:	// Image will be used as a transfer source.						Make sure any reads from and writes to the image have been finished
+				srcMask = srcMask | VK_ACCESS_TRANSFER_READ_BIT;
+				dstMask = VK_ACCESS_TRANSFER_READ_BIT;
+				break;	
+			case VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL			:	// Image will be used as a color attachment.					Make sure any writes to the color buffer have been finished
+				srcMask = VK_ACCESS_TRANSFER_READ_BIT;
+				dstMask = VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT;
+				break; 
+			case VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL			:	// Image will be read in a shader (sampler, input attachment).	Make sure any writes to the image have been finished
 				if (imageMemoryBarrier.srcAccessMask == 0)
-				{
 					imageMemoryBarrier.srcAccessMask = VK_ACCESS_HOST_WRITE_BIT | VK_ACCESS_TRANSFER_WRITE_BIT;
-				}
-				imageMemoryBarrier.dstAccessMask = VK_ACCESS_SHADER_READ_BIT;
+
+				dstMask = VK_ACCESS_SHADER_READ_BIT;
 				break;
 			}
 
